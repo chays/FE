2a19bbcf1c45c2698de83cd3b3d81a55ac095c27
chays <sim22@nate.com>
2019-03-09 22:42:59 +0900

    h up

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path=require('path');
+var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname=url.parse(_url,true).pathname;
-
- 
 
     if(pathname==='/'){
       if(queryData.id===undefined){       
@@ -27,14 +26,16 @@ var app = http.createServer(function(request,response){
       } else {
         fs.readdir('./data',function(error,filelist){
           var filteredId=path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`,'utf8',function(err,discription){     
+          fs.readFile(`data/${filteredId}`,'utf8',function(err,description){     
               var title=queryData.id ;
+              var sanitizedTitle=sanitizeHtml(title);
+              var sanitizedDescription=sanitizeHtml(description)
               var list = template.List(filelist);
-              var html=template.HTML(title,list,`<h2>${title}</h2><p>${discription}</p>`,
+              var html=template.HTML(title,list,`<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
               `<a href="/create">create</a> 
-               <a href="/update?id=${title}">update</a>
+               <a href="/update?id=${sanitizedTitle}">update</a>
                <form action="/delete_process" method="post">
-                <input type="hidden" name="id" value="${title}">
+                <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
                </form>`);
               response.writeHead(200);
